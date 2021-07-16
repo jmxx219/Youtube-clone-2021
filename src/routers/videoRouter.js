aaf8c45f28a131370a0fcda0a7c7c862004aaf94
Jimin <thswlals0219@gmail.com>
2021-07-16 21:03:31 +0900

    CRUD finish

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,15 +4,17 @@ import {
     getEdit, 
     postEdit, 
     getUpload, 
-    postUpload
+    postUpload,
+    deleteVideo
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch); // 정규식(\\d+) 숫자만 받음
-videoRouter.route("/:id/edit").get(getEdit).post(postEdit)
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 // videoRouter.get("/:id(\\d+)/edit", getEdit);
 // videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload)
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
